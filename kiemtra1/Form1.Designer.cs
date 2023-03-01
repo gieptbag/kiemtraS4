2fc7006a3338e0a85a7692f6a2f144ac548b66cc
gieptbag <hiep1308@gmail.com>
2023-03-01 09:06:41 +0700

    tao label mk va texbox mk

--- kiemtra1/Form1.Designer.cs
+++ kiemtra1/Form1.Designer.cs
@@ -29,8 +29,11 @@ namespace kiemtra1
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TextBox txtTenDangNhap;
             this.label1 = new System.Windows.Forms.Label();
-            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.MatKhau = new System.Windows.Forms.Label();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            txtTenDangNhap = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -38,27 +41,45 @@ namespace kiemtra1
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(139, 64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.Size = new System.Drawing.Size(105, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "UserName";
+            this.label1.Text = "Ten dang nhap";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // txtUserName
+            // txtTenDangNhap
             // 
-            this.txtUserName.Location = new System.Drawing.Point(271, 64);
-            this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(175, 22);
-            this.txtUserName.TabIndex = 1;
+            txtTenDangNhap.Location = new System.Drawing.Point(271, 64);
+            txtTenDangNhap.Name = "txtTenDangNhap";
+            txtTenDangNhap.Size = new System.Drawing.Size(175, 22);
+            txtTenDangNhap.TabIndex = 1;
+            // 
+            // MatKhau
+            // 
+            this.MatKhau.AutoSize = true;
+            this.MatKhau.Location = new System.Drawing.Point(142, 112);
+            this.MatKhau.Name = "MatKhau";
+            this.MatKhau.Size = new System.Drawing.Size(64, 17);
+            this.MatKhau.TabIndex = 2;
+            this.MatKhau.Text = "MatKhau";
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(271, 112);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(175, 22);
+            this.txtMatKhau.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.MatKhau);
+            this.Controls.Add(txtTenDangNhap);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "s";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,7 +88,8 @@ namespace kiemtra1
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label MatKhau;
+        private System.Windows.Forms.TextBox txtMatKhau;
     }
 }
 
